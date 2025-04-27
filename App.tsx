bae22407bfc1cfbcd2a28ae5ddc90fe37536a139
GULSAH45 <178339362+GULSAH45@users.noreply.github.com>
2025-04-27 21:58:36 +0300

    finished

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import LoginInputs from 'components/LoginInputs';
 import './global.css';
-import { Text, SafeAreaView, View, Image } from 'react-native';
+import { Text, SafeAreaView, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 mt-[50]">
+    <SafeAreaView className="mt-[50] flex-1">
       <View className="  mt-6 flex-1 items-center">
-        <Text >
+        <Text>
           <Image
             className="mt-4 h-[150px] w-[150px] rounded-full"
             source={require('./assets/panda.png')}
@@ -15,11 +15,14 @@ export default function App() {
         <Text className="mt-5 text-5xl font-bold text-black">Login</Text>
       </View>
 
-      <View className="align-items-center flex-1">
+      <View className="align-items-center flex-1 mt-[-300] mb-[50]">
         <LoginInputs label="Your Name" placeholder="Enter your name" type="text" />
         <LoginInputs label="Email" placeholder="Enter your email" type="text" />
         <LoginInputs label="Password" placeholder="Enter your password" type="password" />
       </View>
+      <TouchableOpacity>
+        <Text className='text-center rounded-full text-white font-bold text-xl m-5 p-1 h-[40px] bg-orange-500'>GO</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
--- components/LoginInputs.tsx
+++ components/LoginInputs.tsx
@@ -2,7 +2,7 @@ import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import Feather from '@expo/vector-icons/Feather';
 
-const LoginInputs = (props: { label: string; placeholder: string; type: text | password }) => {
+const LoginInputs = (props: { label: string; placeholder: string; type: 'text' | 'password' }) => {
   const [Issecure, setIssecure] = useState(true);
   const handlesecurity = () => {
     setIssecure(!Issecure);
@@ -10,23 +10,27 @@ const LoginInputs = (props: { label: string; placeholder: string; type: text | p
 
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-bold">{props.label}</Text>
-      <TextInput
-        placeholder={props.placeholder}
-        placeholderTextColor={'orange'}
-        secureTextEntry={props.type === 'password' ? Issecure : false}
-        className="mb-4 mt-1 w-[250] rounded-md border-2 border-gray-300 p-2"
-      />
+      <Text className="font-bold">{props.label}</Text>
+      <View className="relative">
+        <TextInput
+          placeholder={props.placeholder}
+          placeholderTextColor={'orange'}
+          secureTextEntry={props.type === 'password' ? Issecure : false}
+          className="mb-4 mt-1 w-[250px] rounded-md border-2 border-gray-300 p-2"
+        />
 
-      {props.type === 'password ' && (
-        <TouchableOpacity classname="absolute right-2 top-2" onPress={handlesecurity}>
-          {Issecure ? (
-            <Feather name="eye-off" size={24} color="black" />
-          ) : (
-            <Feather name="eye" size={24} color="black" />
-          )}
-        </TouchableOpacity>
-      )}
+        {props.type === 'password' && (
+          <TouchableOpacity className="absolute bottom-5 right-2" onPress={handlesecurity}>
+            {Issecure ? (
+              <Feather name="eye-off" size={24} color="black" />
+            ) : (
+              <Feather name="eye" size={24} color="black" />
+            )}
+          </TouchableOpacity>
+        )}
+      </View>
+
+
     </View>
   );
 };
