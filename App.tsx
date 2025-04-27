015282cc08b783da3da11c5c60132f016050cdc5
GULSAH45 <178339362+GULSAH45@users.noreply.github.com>
2025-04-27 21:21:30 +0300

    UNDONE

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import LoginInputs from 'components/LoginInputs';
 import './global.css';
+import { Text, SafeAreaView, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <SafeAreaView className="flex-1 mt-[50]">
+      <View className="  mt-6 flex-1 items-center">
+        <Text >
+          <Image
+            className="mt-4 h-[150px] w-[150px] rounded-full"
+            source={require('./assets/panda.png')}
+          />
+        </Text>
+        <Text className="mt-5 text-5xl font-bold text-black">Login</Text>
+      </View>
+
+      <View className="align-items-center flex-1">
+        <LoginInputs label="Your Name" placeholder="Enter your name" type="text" />
+        <LoginInputs label="Email" placeholder="Enter your email" type="text" />
+        <LoginInputs label="Password" placeholder="Enter your password" type="password" />
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/LoginInputs.tsx
@@ -0,0 +1,34 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import Feather from '@expo/vector-icons/Feather';
+
+const LoginInputs = (props: { label: string; placeholder: string; type: text | password }) => {
+  const [Issecure, setIssecure] = useState(true);
+  const handlesecurity = () => {
+    setIssecure(!Issecure);
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-bold">{props.label}</Text>
+      <TextInput
+        placeholder={props.placeholder}
+        placeholderTextColor={'orange'}
+        secureTextEntry={props.type === 'password' ? Issecure : false}
+        className="mb-4 mt-1 w-[250] rounded-md border-2 border-gray-300 p-2"
+      />
+
+      {props.type === 'password ' && (
+        <TouchableOpacity classname="absolute right-2 top-2" onPress={handlesecurity}>
+          {Issecure ? (
+            <Feather name="eye-off" size={24} color="black" />
+          ) : (
+            <Feather name="eye" size={24} color="black" />
+          )}
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default LoginInputs;
